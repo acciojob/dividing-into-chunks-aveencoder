a402a673d02960659e40b46b531e2d94ddb08ae9
aveencoder <aveen598@gmail.com>
2025-08-07 20:08:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-   let result = [];
-    let i=0;
-    while(i<arr.length){
-      let sum = 0;
-       let temp =[];
-       for(let j=i;j<arr.length;j++){
-        sum = sum+ arr[j];
-        if(sum>n){
-            i = j;
-             break;
-        }  
-           temp.push(arr[j]);
-       }
-       result.push(temp);
+ const result = [];
+  let i = 0;
+
+  while (i < arr.length) {
+    let sum = 0;
+    const temp = [];
+
+    // Try to consume elements until adding one exceeds the limit
+    while (i < arr.length && sum + arr[i] <= n) {
+      sum += arr[i];
+      temp.push(arr[i]);
+      i++;
     }
-    return result;
+
+    // If no elements were added (i.e., arr[i] > n), we can still push that single element or handle differently
+    if (temp.length === 0 && i < arr.length) {
+      // Optional: choose to include the non-fitting element alone
+      temp.push(arr[i]);
+      i++;
+    }
+
+    result.push(temp);
+	   }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
